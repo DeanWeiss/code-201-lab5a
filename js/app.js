316e4ad847e53dc1c08bc46ae5a4424addc945c6
Dean William Weiss <dean.w.weiss@gmail.com>
2022-04-08 17:28:25 -0700

    completed question three, test passing in console.

--- js/app.js
+++ js/app.js
@@ -52,11 +52,19 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function sumAndMultiply(a, b, c) { //eslint-disable-line
+    let numSum = sum(a,b)[0];
+    let numSum2 = sum(numSum,c)[0];
 
+    let numMult = multiply(a,b)[0];
+    let numMult2 = multiply(numMult,c)[0];
+
+    let str = `${a} and ${b} and ${c} sum to ${numSum2}.`;
+    let product = `The product of ${a} and ${b} and ${c} is ${numMult2}.`;
+    return [numSum2, numMult2, str, product];
 }
 
 // Here is the test for sumAndMultiply(); uncomment it to run it
-// testSumAndMultiply(4,7,5);
+testSumAndMultiply(4,7,5);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
